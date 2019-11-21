3806cf4c5780828e8120af0f3233f255c3e3c2b9
pouya-moh <other.pouya@gmail.com>
2019-11-21 10:38:44 +0100

    add documentation

--- include/KinDynModel.hpp
+++ include/KinDynModel.hpp
@@ -2,10 +2,25 @@
 #define KINDYNMODEL_HPP
 
 
-class KinDynModel
-{
+/**
+ * @brief The KinDynModel class
+ * This is pure abstract class that acts as interface factory. The idea is that
+ * its pure abstract methods are implemented in classes that extend it. These
+ * classes will implement said functionality using different kinematic/dynamic
+ * solvers, e.g., RBDL, KDL, etc.
+ *
+ * ##CONVENTIONS##
+ * Although compiler optimizations makes the return by copy very efficient, since
+ * most libraries still prefer to return the results by reference via argument,
+ * I will adhere to the latter. This means, for operations it might be necessary
+ * to do some internal work to make it happen. RBDL for instance, is a bit
+ * inconsistent in this regards.
+ */
+class KinDynModel {
 public:
-    KinDynModel();
+//    KinDynModel();
+
+
 };
 
 #endif // KINDYNMODEL_HPP
