502e9fba24d06f1117847cb4116fffe3b091476d
pouya-moh <other.pouya@gmail.com>
2019-11-27 11:42:38 +0100

    add header for compuation of CoM

--- src/KinDynModelRBDL.cpp
+++ src/KinDynModelRBDL.cpp
@@ -8,6 +8,7 @@
 //#include <KinDynModelRBDL.hpp>
 #include "KinDynModelRBDL.hpp"
 #include <rbdl/addons/urdfreader/urdfreader.h>
+#include <rbdl/rbdl_utils.h>
 #include <iostream>
 #include <rbdl/Kinematics.h>
 
